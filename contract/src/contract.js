13e2c8b9125c34efe5d0b56ebde7c266b7f47b08
Dan Connolly <dckc@madmode.com>
2021-12-06 13:28:39 -0600

    style(contract): lint

--- contract/src/contract.js
+++ contract/src/contract.js
@@ -1,10 +1,10 @@
 // @ts-check
 import { Far } from '@agoric/marshal';
 
-const start = async (zcf) => {
+const start = async (_zcf) => {
   let value = 'Hello, World!';
   const creatorFacet = Far('hello', {
-    set: v => (value = v),
+    set: (v) => (value = v),
   });
   const publicFacet = Far('hello', {
     get: () => value,
