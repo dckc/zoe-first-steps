9573a2b7b3c147dcd7d053064b564e2e5a444bb8
Dan Connolly <dckc@madmode.com>
2021-12-07 01:58:12 -0600

    feat: deploy peg based on channel id

--- contract/src/deploy-peg.js
+++ contract/src/deploy-peg.js
@@ -2,10 +2,15 @@ import { E } from '@agoric/eventual-send';
 
 // const { details: X } = assert;
 
-const CosmosHubTestnet = harden({
-  keyword: 'Photon',
-  denom: 'uphoton',
-  decimalPlaces: 6,
+const chains = harden({
+  cosmos: {
+    keyword: 'Photon',
+    denom: 'uphoton',
+    decimalPlaces: 6,
+  },
+  agoric: {
+    channel: 'channel-10',
+  },
 });
 
 /**
@@ -24,19 +29,21 @@ const deployPeg = async (homeP, _powers) => {
 
   assert(home.pegasusConnections, `home.pegasusConnections power missing`);
   console.log('awaiting pegasusConnections...');
-  const connections = await E(home.pegasusConnections).values();
+  const connections = await E(home.pegasusConnections).entries();
   assert(connections.length > 0, `pegasusConnections nameHub is empty`);
-  console.log('pegasusConnections:', connections);
-  const [conn] = connections.slice(-1); // last item
+  console.log('pegasusConnections:', connections.length);
+  const [addr, conn] = connections.find(([a, _c]) =>
+    a.endsWith(chains.agoric.channel),
+  );
 
   console.log('getting instance, publicFacet');
   /** @type { Instance } */
   const instance = await E(home.agoricNames).lookup('instance', 'Pegasus');
   const pegPub = E(home.zoe).getPublicFacet(instance);
 
-  const { keyword, denom, decimalPlaces } = CosmosHubTestnet;
-  const name = `peg-${connections.length}-${denom}`;
-  console.log('creating', name);
+  const { keyword, denom, decimalPlaces } = chains.cosmos;
+  const name = `peg-${chains.agoric.channel}-${denom}`;
+  console.log('creating', name, 'from', addr);
   const NAT = 'nat'; // AssetKind.NAT from ERTP
   const peg = await E(pegPub).pegRemote(keyword, conn, denom, NAT, {
     decimalPlaces,
@@ -47,7 +54,7 @@ const deployPeg = async (homeP, _powers) => {
   const brand = await E(peg).getLocalBrand();
   const issuer = await E(pegPub).getLocalIssuer(brand);
   const issuerBoardId = await E(home.board).getId(issuer);
-  console.log({ issuerBoardId }, CosmosHubTestnet);
+  console.log({ issuerBoardId }, chains);
 };
 
 harden(deployPeg);
